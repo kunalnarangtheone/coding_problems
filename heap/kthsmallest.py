c6171ee2a1d26065bd5c10cf1bcd26bda047d729
Kunal Narang <kunalnarang1994@gmail.com>
2023-06-19 15:11:18 -0700

    Added kth largest and smallest code

---
+++ heap/kthsmallest.py
@@ -0,0 +1,25 @@
+from heapq import heappush, heappop
+
+def kthlargest(nums, k):
+    assert len(nums) > 0, "nums array should exist!"
+    assert k > 0, "k should be a positive integer!"
+    assert k <= len(nums), "k should be smaller than or equal to the length of the array!"
+
+    heap = []
+    
+    for num in nums:
+        if len(heap) < k:
+            heappush(heap, num)
+            continue
+
+        if num > heap[0]:
+            heappop(heap)
+            heappush(heap, num)
+
+    return heap[0]
+
+
+print(kthlargest([1, 5, 2, 9, 4, 3], 3))
+
+
+    
