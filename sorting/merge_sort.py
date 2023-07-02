1830a8d1024fd3b83d13a2ec02469f9f64aeae75
Kunal Narang <kunalnarang1994@gmail.com>
2023-07-02 10:55:13 -0700

    Added merge sort

---
+++ sorting/merge_sort.py
@@ -0,0 +1,43 @@
+
+def merge_sort(array):
+    l = 0
+    r = len(array) - 1
+
+    if l < r:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+        m, n = len(left), len(right)
+
+        while i < m and j < n:
+            if left[i] <= right[j]:
+                array[k] = left[i]
+                i+=1
+
+            else:
+                array[k] = right[j]
+                j+=1
+
+            k+=1
+
+        while i < m:
+            array[k] = left[i]
+            i+=1
+            k+=1
+
+        while j < n:
+            array[k] = right[j]
+            j+=1
+            k+=1
+
+arr = [1, 5, 2, 7, 3, 8, 2]
+merge_sort(arr)
+print(arr)
+
+
+
+
